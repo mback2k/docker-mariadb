e1251bf07625276a1b74d85a2aa096dd10df5864
Marc Hoersken <info@marc-hoersken.de>
2018-06-22 15:46:14 +0200

    Load timezone information using mysql_embedded command prior start

---
+++ docker-entrypoint.d/05-load-timezones.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -e
+
+if [ -z "${MYSQL_INITDB_SKIP_TZINFO}" ]; then
+if [ -d "/usr/share/zoneinfo" ]; then
+    runuser -u mysql -- /bin/sh -c 'mysql_tzinfo_to_sql /usr/share/zoneinfo | mysql_embedded mysql'
+fi
+fi
+
+exit 0
