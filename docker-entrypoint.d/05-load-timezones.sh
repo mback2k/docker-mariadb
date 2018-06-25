fd463ac303c7adeac85e4b7433826d774cf8363c
Marc Hoersken <info@marc-hoersken.de>
2018-06-25 19:52:18 +0200

    Because of mysqldump as SST method we need to initialize the DB

--- docker-entrypoint.d/05-load-timezones.sh
+++ docker-entrypoint.d/05-load-timezones.sh
@@ -2,10 +2,8 @@
 set -e
 
 if [ -z "${MYSQL_INITDB_SKIP_TZINFO}" ]; then
-if [ -z "${WSREP_CLUSTER_ROLE}" -o "${WSREP_CLUSTER_ROLE}" = "seed" ]; then
 if [ -d "/usr/share/zoneinfo" -a -d "mysql/mysql" ]; then
     runuser -u mysql -- /bin/sh -c 'mysql_tzinfo_to_sql /usr/share/zoneinfo | mysql_embedded mysql'
-fi
 fi
 fi
 
