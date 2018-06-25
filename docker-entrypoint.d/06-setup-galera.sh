119e3aff091a288ce6b00fb7c72717611db57533
Marc Hoersken <info@marc-hoersken.de>
2018-06-25 19:41:24 +0200

    Use mysqldump instead of rsync as SST method

--- docker-entrypoint.d/06-setup-galera.sh
+++ docker-entrypoint.d/06-setup-galera.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 set -e
 
+if [ -f "${MYSQL_ROOT_PASSWORD_FILE}" ]; then
+    MYSQL_ROOT_PASSWORD="$(< "${MYSQL_ROOT_PASSWORD_FILE}")"
+fi
+
+if [ -n "${MYSQL_ROOT_PASSWORD}" ]; then
 if [ -n "${WSREP_CLUSTER_ADDRESS}" ]; then
 if [ -n "${WSREP_CLUSTER_NAME}" ]; then
 if [ -n "${WSREP_NODE_ADDRESS}" ]; then
 if [ -n "${WSREP_NODE_NAME}" ]; then
+    touch /etc/mysql/conf.d/docker-galera.cnf
+    chown root:mysql /etc/mysql/conf.d/docker-galera.cnf
+    chmod 640 /etc/mysql/conf.d/docker-galera.cnf
     WSREP_NODE_ADDRESS=$(getent hosts $(hostname) | awk '{print $1}' | grep -e "${WSREP_NODE_ADDRESS}")
     echo "[galera]" > /etc/mysql/conf.d/docker-galera.cnf
     echo "wsrep_on=ON" >> /etc/mysql/conf.d/docker-galera.cnf
@@ -13,7 +21,8 @@ if [ -n "${WSREP_NODE_NAME}" ]; then
     echo "wsrep_cluster_name='${WSREP_CLUSTER_NAME}'" >> /etc/mysql/conf.d/docker-galera.cnf
     echo "wsrep_node_address='${WSREP_NODE_ADDRESS}'" >> /etc/mysql/conf.d/docker-galera.cnf
     echo "wsrep_node_name='${WSREP_NODE_NAME}'" >> /etc/mysql/conf.d/docker-galera.cnf
-    echo "wsrep_sst_method=rsync" >> /etc/mysql/conf.d/docker-galera.cnf
+    echo "wsrep_sst_auth='root:${MYSQL_ROOT_PASSWORD}'" >> /etc/mysql/conf.d/docker-galera.cnf
+    echo "wsrep_sst_method=mysqldump" >> /etc/mysql/conf.d/docker-galera.cnf
     echo "binlog_format=ROW" >> /etc/mysql/conf.d/docker-galera.cnf
     echo "log_bin=/var/log/mysql/mysql-bin.log" >> /etc/mysql/conf.d/docker-galera.cnf
     echo "log_slave_updates" >> /etc/mysql/conf.d/docker-galera.cnf
@@ -25,5 +34,8 @@ fi
 fi
 fi
 fi
+fi
+
+unset MYSQL_ROOT_PASSWORD
 
 exit 0
