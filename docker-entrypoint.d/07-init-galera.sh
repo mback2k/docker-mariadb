b52fb8574bd8699f9521d5d72af8a3ea1ff4f706
Marc Hoersken <info@marc-hoersken.de>
2018-12-03 19:57:40 +0100

    Only allow mysql group to read credentials in galera mode

--- docker-entrypoint.d/07-init-galera.sh
+++ docker-entrypoint.d/07-init-galera.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 set -e
 
-if [ -f "/etc/mysql/conf.d/docker-galera.cnf" ]; then
+if [ ! -f "/etc/mysql/conf.d/docker-galera.cnf" ]; then
+    exit 0
+fi
+
 if [ -n "${WSREP_ROLE}" -a "${WSREP_ROLE}" = "seed" -a ! -s "/var/lib/mysql/grastate.dat" ] || \
    grep -e "^safe_to_bootstrap: 1$" "/var/lib/mysql/grastate.dat"; then
     echo "wsrep_new_cluster" >> /etc/mysql/conf.d/docker-galera.cnf
-fi
 fi
 
 echo "Undefined" > /var/lib/mysql/docker-galera.state
 
 chown mysql:mysql /var/lib/mysql/docker-galera.state
 chmod 660 /var/lib/mysql/docker-galera.state
+
+chown root:mysql /etc/mysql/debian.cnf
+chmod 640 /etc/mysql/debian.cnf
 
 exit 0
